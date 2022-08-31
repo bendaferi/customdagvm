398e939bed075d7f52e8a6fe6f836ed5ab09bac4
Benda Ferenc <feriapro91@gmail.com>
2022-08-31 19:56:06 +0200

    Initial commit

---
+++ main/main.go
@@ -0,0 +1,29 @@
+// (c) 2019-2020, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	log "github.com/inconshreveable/log15"
+	// "github.com/bendaferi/customdagvm"
+)
+
+func main() {
+	version, err := PrintVersion()
+	if err != nil {
+		fmt.Printf("couldn't get config: %s", err)
+		os.Exit(1)
+	}
+	// Print VM ID and exit
+	if version {
+		// fmt.Printf("%s@%s\n", customdagvm.Name, customdagvm.Version)
+		os.Exit(0)
+	}
+
+	log.Root().SetHandler(log.LvlFilterHandler(log.LvlDebug, log.StreamHandler(os.Stderr, log.TerminalFormat())))
+
+	// rpcdagvm.Serve(&customdagvm.VM{})
+}
